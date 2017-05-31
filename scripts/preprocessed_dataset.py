efddb1970d55ca2051c20cab674c500103948e80
Yusuke Oshiro <la.fete.du.travail@gmail.com>
2017-06-01 01:55:19 +0900

    do not use depth image from pcl_pcd2png

--- scripts/preprocessed_dataset.py
+++ scripts/preprocessed_dataset.py
@@ -62,7 +62,6 @@ class PreprocessedDataset(dataset.DatasetMixin):
         c_idx_format = 'object_' + '{0:02d}'.format(c_idx)
         c_path = os.path.join(self.base, c_idx_format)
         rgb = cv2.imread(os.path.join(c_path, 'rgb_' + v_idx_format +'.png'))
-        depth = cv2.imread(os.path.join(c_path, 'depth_' + v_idx_format +'.png'), 0)
         mask = cv2.imread(os.path.join(c_path, 'mask_' + v_idx_format +'.png'))
         pos = np.load(os.path.join(c_path, 'pos_' + v_idx_format +'.npy'))
         rot = np.load(os.path.join(c_path, 'rot_' + v_idx_format +'.npy'))
@@ -70,13 +69,13 @@ class PreprocessedDataset(dataset.DatasetMixin):
         # quat = calc_quaternion(rot)
         # rpy = rpy_param(rot)
         pc = np.load(os.path.join(c_path, 'pc_' + v_idx_format +'.npy'))
-        return rgb, depth, mask, pos, rot, pc
+        return rgb, mask, pos, rot, pc
 
     def get_example(self, i):
         img_size = self.img_size
         c_i = self.class_indices[i // self.n_view]
         v_i = self.view_indices[i % self.n_view]
-        img_rgb, img_depth, mask, pos, rot, pc = self.load_orig_data(c_i, v_i)
+        img_rgb, mask, pos, rot, pc = self.load_orig_data(c_i, v_i)
 
         # image, label = self.base[i]
         # _, h, w = image.shape
@@ -113,7 +112,7 @@ class PreprocessedDataset(dataset.DatasetMixin):
             rand_h = random.randint(0,40)
             rand_w = random.randint(0,40)
             img_rgb = img_rgb[(120+rand_h):(120+192+rand_h), (140+rand_w):(140+256+rand_w)]
-            img_depth = img_depth[(120+rand_h):(120+192+rand_h), (140+rand_w):(140+256+rand_w)]
+            img_depth = pc[(120+rand_h):(120+192+rand_h), (140+rand_w):(140+256+rand_w)]
             mask = mask[(120+rand_h):(120+192+rand_h), (140+rand_w):(140+256+rand_w)]
             pc = pc[(120+rand_h):(120+192+rand_h), (140+rand_w):(140+256+rand_w)]
         else:
@@ -125,6 +124,16 @@ class PreprocessedDataset(dataset.DatasetMixin):
         img_rgb = img_rgb / 255.0  # Scale to [0, 1];
         img_rgb = cv2.resize(img_rgb, img_size)
         img_rgb = img_rgb.transpose(2,0,1).astype(np.float32)
+
+        img_depth = np.square(img_depth).sum(axis=2)
+        img_depth[img_depth != img_depth] = 0
+        img_depth = np.sqrt(img_depth)
+
+        # only consider range 0.5 ~ 4.5[m]
+        img_depth = (img_depth - 0.5) / 4.0
+        img_depth[img_depth > 1] = 1
+        img_depth[img_depth < 0] = 0
+
         img_depth = img_depth / 255.0  # Scale to [0, 1];
         img_depth =  cv2.resize(img_depth, img_size)
         img_depth = img_depth.reshape(1, img_size[1], img_size[0]).astype(np.float32)
