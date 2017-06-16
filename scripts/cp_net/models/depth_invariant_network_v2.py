0d410bf9d1509b93a44b77cf7b9c7c5041757195
Yusuke Oshiro <la.fete.du.travail@gmail.com>
2017-06-16 16:26:15 +0900

    [tmp]add di net ver2

--- scripts/cp_net/models/depth_invariant_network_v2.py
+++ scripts/cp_net/models/depth_invariant_network_v2.py
@@ -10,7 +10,7 @@ from chainer.functions.array import concat
 
 from cp_net.functions.convolutional_roi_pooling import convolutional_roi_pooling
 
-class DepthInvariantNetworkRes50FCN(chainer.Chain):
+class DepthInvariantNetworkRes50FCNVer2(chainer.Chain):
     def __init__(self, n_class=36, pretrained_model = None):
         if pretrained_model:
             # As a sampling process is time-consuming,
@@ -20,22 +20,20 @@ class DepthInvariantNetworkRes50FCN(chainer.Chain):
             # employ default initializers used in the original paper
             kwargs = {'initialW': normal.HeNormal(scale=1.0)}
         self.n_class = n_class
-        super(DepthInvariantNetworkRes50FCN, self).__init__(
+        super(DepthInvariantNetworkRes50FCNVer2, self).__init__(
             # resnet50
             conv1=L.Convolution2D(3, 64, 7, 2, 3, **kwargs),
             bn1=L.BatchNormalization(64),
             res2=R.BuildingBlock(3, 64, 64, 256, 1, **kwargs), # resblock 1/2 -> 1/4
             res3=R.BuildingBlock(4, 256, 128, 512, 2, **kwargs), # resblock 1/4 ->1/8
             res4=R.BuildingBlock(6, 512, 256, 1024, 2, **kwargs), # resblock 1/8 -> 1/16
-            res5=R.BuildingBlock(3, 1024, 512, 2048, 2, **kwargs), # resblock 1/16 -> 1/32
 
-            upscore1=L.Deconvolution2D(2048, 512, 16, stride=8, pad=4),
-            upscore2=L.Deconvolution2D(1024, 512, 8,stride=4, pad=2),
-            upscore3=L.Deconvolution2D(512, 512, 4, stride=2, pad=1),
+            upscore1=L.Deconvolution2D(1024, 512, 8,stride=4, pad=2),
+            upscore2=L.Deconvolution2D(512, 512, 4, stride=2, pad=1),
 
-            bn_upscore = L.BatchNormalization(512 * 3),
+            bn_upscore = L.BatchNormalization(512 * 2 + 256),
 
-            concat_conv = L.Convolution2D(512*3,  1024, 3, stride=1, pad=1),
+            concat_conv = L.Convolution2D(512*2 + 256,  1024, 3, stride=1, pad=1),
 
             pool_roi_conv =  L.Convolution2D(1024, 1024, 5, stride=5, pad=0),
             conv_after_croip = L.Convolution2D(1024, 512, 3, stride=1, pad=1),
@@ -56,22 +54,17 @@ class DepthInvariantNetworkRes50FCN(chainer.Chain):
         h = F.max_pooling_2d(h, 3, stride=2)
         # Res Blocks
         h = self.res2(h) # 1/4
+        pool1_4 = h
         h = self.res3(h) # 1/8
-        pool1_8 = h
-        h = self.res4(h) # 1/16
-        pool1_16 = h
-        h = self.res5(h) # 1/32
-        pool1_32 = h
-
-        # upscore 1/32 -> 1/4
-        pool1_32 = self.upscore1(pool1_32)
-        # upscore 1/16 -> 1/4
-        pool1_16 = self.upscore2(pool1_16)
         # upscore 1/8 -> 1/4
-        pool1_8 = self.upscore3(pool1_8)
+        pool1_8 = self.upscore2(h)
+
+        h = self.res4(h) # 1/16
+        # upscore 1/16 -> 1/4
+        h = self.upscore1(h)
 
         # concat 1 / 4
-        h = F.leaky_relu(self.bn_upscore(concat.concat((pool1_32, pool1_16, pool1_8), axis=1)))
+        h = F.leaky_relu(self.bn_upscore(concat.concat((h, pool1_8, pool1_4), axis=1)))
         h = F.relu(self.concat_conv(h))
 
         ksizes = F.ceil(F.resize_images((ksizes * 3), (h.data.shape[2], h.data.shape[3])))
