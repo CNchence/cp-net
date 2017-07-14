1d9e5573f61b2dd3added905a4f33c5846cc4224
Yusuke Oshiro <la.fete.du.travail@gmail.com>
2017-07-14 20:44:47 +0900

    move trainer to examples

--- scripts/examples/willow_models/train.py
+++ scripts/examples/willow_models/train.py
@@ -19,15 +19,15 @@ import chainer.links.model.vision.resnet as R
 from chainer.training import extensions
 from chainer.links.caffe import CaffeFunction
 
-from cp_net.models.depth_invariant_network import DepthInvariantNetworkRes50FCN
-from cp_net.models.depth_invariant_network_v2 import DepthInvariantNetworkRes50FCNVer2
-from cp_net.di_net_dataset import DepthInvariantNetDataset
+from cp_net.models.cp_network import CenterProposalNetworkRes50FCN
+from cp_net.cp_classifier import CPNetClassifier
+from cp_net.preprocessed_dataset import PreprocessedDataset
 
 import argparse
 import os
 import numpy as np
 
-def _transfer_pretrain_resnet50(src, dst, use_res5=True):
+def _transfer_pretrain_resnet50(src, dst):
     dst.conv1.W.data[:] = src.conv1.W.data
     dst.conv1.b.data[:] = src.conv1.b.data
     dst.bn1.avg_mean[:] = src.bn_conv1.avg_mean
@@ -38,20 +38,15 @@ def _transfer_pretrain_resnet50(src, dst, use_res5=True):
     R._transfer_block(src, dst.res2, ['2a', '2b', '2c'])
     R._transfer_block(src, dst.res3, ['3a', '3b', '3c', '3d'])
     R._transfer_block(src, dst.res4, ['4a', '4b', '4c', '4d', '4e', '4f'])
-    if use_res5:
-        R._transfer_block(src, dst.res5, ['5a', '5b', '5c'])
+    R._transfer_block(src, dst.res5, ['5a', '5b', '5c'])
 
-def _make_chainermodel_npz(path_npz, path_caffemodel, model, num_class, v2=True):
+def _make_chainermodel_npz(path_npz, path_caffemodel, model, num_class):
     print('Now loading caffemodel (usually it may take few minutes)')
     if not os.path.exists(path_caffemodel):
         raise IOError('The pre-trained caffemodel does not exist.')
     caffemodel = CaffeFunction(path_caffemodel)
-    if v2:
-        chainermodel = DepthInvariantNetworkRes50FCNVer2(n_class=num_class)
-        _transfer_pretrain_resnet50(caffemodel, chainermodel, use_res5=False)
-    else:
-        chainermodel = DepthInvariantNetworkRes50FCN(n_class=num_class)
-        _transfer_pretrain_resnet50(caffemodel, chainermodel, use_res5=True)
+    chainermodel = CenterProposalNetworkRes50FCN(n_class=num_class)
+    _transfer_pretrain_resnet50(caffemodel, chainermodel)
     classifier_model = L.Classifier(chainermodel)
     serializers.save_npz(path_npz, classifier_model, compression=False)
     print('model npz is saved')
@@ -60,14 +55,14 @@ def _make_chainermodel_npz(path_npz, path_caffemodel, model, num_class, v2=True)
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Fully Convolutional Depth Invariant Network')
+    parser = argparse.ArgumentParser(description='Fully Convolutional Center Pose Proposal Network for Pose Estimation')
     parser.add_argument('--batchsize', '-b', type=int, default=1,
                         help='Number of images in each mini-batch')
     parser.add_argument('--epoch', '-e', type=int, default=200,
                         help='Number of sweeps over the dataset to train')
     parser.add_argument('--gpu', '-g', type=int, default=-1,
                         help='GPU ID (negative value indicates CPU)')
-    parser.add_argument('--out', '-o', default='di_net_result',
+    parser.add_argument('--out', '-o', default='result',
                         help='Directory to output the result')
     parser.add_argument('--resume', '-r', default='',
                         help='Resume the training from snapshot')
@@ -81,8 +76,6 @@ def main():
                         help='Frequency of taking a snapshot')
     parser.add_argument('--train_resnet', type=bool, default=False,
                         help='train resnet')
-    parser.add_argument('--ver2', type=bool, default=True,
-                        help='di net version 2')
 
     args = parser.parse_args()
 
@@ -94,19 +87,12 @@ def main():
     n_class = 2
     # n_class = 36
     n_view = 10
-    train_path = os.path.join(os.getcwd(), '../train_data/willow_models')
+    train_path = os.path.join(os.getcwd(), '../../../train_data/willow_models')
     caffe_model = 'ResNet-50-model.caffemodel'
 
-    chainer.using_config('cudnn_deterministic', True)
-
-    if args.ver2:
-        model = L.Classifier(
-            DepthInvariantNetworkRes50FCNVer2(n_class=n_class,
-                                              pretrained_model= not args.train_resnet))
-    else:
-        model = L.Classifier(
-            DepthInvariantNetworkRes50FCN(n_class=n_class,
-                                          pretrained_model= not args.train_resnet))
+    
+    model = CPNetClassifier(CenterProposalNetworkRes50FCN(n_class=n_class,
+                                                          pretrained_model= not args.train_resnet))
 
     if args.gpu >= 0:
         chainer.cuda.get_device(args.gpu).use()  # Make a specified GPU current
@@ -117,23 +103,14 @@ def main():
     optimizer.setup(model)
 
     # load train data
-    train = DepthInvariantNetDataset(train_path, range(1,n_class), range(0, n_view - 2),
-                                     random_resize=True, resize_train=True)
+    train = PreprocessedDataset(train_path, range(1,n_class), range(0, n_view - 2))
     # load test data
-    test = DepthInvariantNetDataset(train_path, range(1,n_class), range(n_view - 2, n_view),
-                                    img_size=(256, 192), random=False, random_flip=False)
-
-    test_resized = DepthInvariantNetDataset(train_path, range(1,n_class),
-                                            range(n_view - 2, n_view),
-                                            img_size=(256, 192), random=False,
-                                            random_flip=False, random_resize=True,
-                                            force_resize=True)
+    test = PreprocessedDataset(train_path, range(1,n_class), range(n_view - 2, n_view),
+                               img_size=(256, 192), random=False)
 
     train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
     test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
                                                  repeat=False, shuffle=False)
-    test_resized_iter = chainer.iterators.SerialIterator(test_resized, args.batchsize,
-                                                         repeat=False, shuffle=False)
 
     updater = training.StandardUpdater(train_iter, optimizer, device=args.gpu)
     trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
@@ -143,10 +120,6 @@ def main():
     evaluator = extensions.Evaluator(test_iter, model, device=args.gpu)
     evaluator.default_name = 'val'
     trainer.extend(evaluator)
-
-    evaluator_resized = extensions.Evaluator(test_resized_iter, model, device=args.gpu)
-    evaluator_resized.default_name = 'resized_val'
-    trainer.extend(evaluator_resized)
 
     # The "main" refers to the target link of the "main" optimizer.
     trainer.extend(extensions.dump_graph('main/loss'))
@@ -169,9 +142,11 @@ def main():
         #         'epoch', file_name='accuracy.png'))
 
     trainer.extend(extensions.PrintReport(
-        ['epoch',  'main/loss', 'main/accuracy',
-         'val/main/loss','val/main/accuracy',
-         'resized_val/main/loss','resized_val/main/accuracy',
+
+        ['epoch',  'main/c_loss',  'main/p_loss', 'main/r_loss',
+         'main/c_acc', 'main/p_acc', 'main/r_acc',
+         'val/main/c_loss',  'val/main/p_loss', 'val/main/r_loss',
+         'val/main/c_acc', 'val/main/p_acc', 'val/main/r_acc',
          'elapsed_time']))
 
 
@@ -182,11 +157,8 @@ def main():
         # Resume from a snapshot
         chainer.serializers.load_npz(args.resume, trainer)
     else:
-        root = '..'
-        if args.ver2:
-            npz_name = 'DepthInvariantNetworkRes50FCNVer2.npz'
-        else:
-            npz_name = 'DepthInvariantNetworkRes50FCN.npz'
+        root = '../../../'
+        npz_name = 'CenterProposalNetworkRes50FCN.npz'
         caffemodel_name = 'ResNet-50-model.caffemodel'
         path = os.path.join(root, 'trained_data/', npz_name)
         path_caffemodel = os.path.join(root, 'trained_data/', caffemodel_name)
@@ -194,8 +166,7 @@ def main():
         print 'caffe model path : ' + path_caffemodel
         download.cache_or_load_file(
             path,
-            lambda path: _make_chainermodel_npz(path, path_caffemodel,
-                                                model, n_class, v2=args.ver2),
+            lambda path: _make_chainermodel_npz(path, path_caffemodel, model, n_class),
             lambda path: serializers.load_npz(path, model))
 
     # Run the training
@@ -204,3 +175,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
