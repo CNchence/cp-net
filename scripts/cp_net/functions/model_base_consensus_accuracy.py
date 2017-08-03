70a53cf138b6b108a010ff8aa295ae70e577e081
oshiro <la.fete.du.travail@gmail.com>
2017-08-03 18:32:48 +0900

    modify model_base_consensus

--- scripts/cp_net/functions/model_base_consensus_accuracy.py
+++ scripts/cp_net/functions/model_base_consensus_accuracy.py
@@ -3,6 +3,7 @@ import quaternion
 import six
 
 import pypcd
+import pyflann
 
 from chainer import cuda
 from chainer import function
@@ -16,72 +17,6 @@ def calc_rot_by_svd(Y, X):
     H = np.diag(np.array([1, 1, VU_det], dtype=np.float64))
     R = np.dot(np.dot(U, H), V)
     return R
-
-def svd_estimation(y_ocp_nonzero, t_pc_nonzero, n_iter=2):
-    ## iterative SVD
-    diff_mask = np.ones(t_pc_nonzero.shape[1]).astype(np.bool)
-    for j in six.moves.range(n_iter):
-        if np.sum(diff_mask) >= 3:
-            y_ocp_nonzero = y_ocp_nonzero[:, diff_mask]
-            y_ocp_mean = np.mean(y_ocp_nonzero, axis=1, keepdims=True)
-            y_ocp_demean = y_ocp_nonzero - y_ocp_mean
-
-            t_pc_nonzero = t_pc_nonzero[:, diff_mask]
-            t_pc_mean = np.mean(t_pc_nonzero, axis=1, keepdims=True)
-            t_pc_demean = t_pc_nonzero - t_pc_mean
-
-            ret_R = calc_rot_by_svd(t_pc_demean, y_ocp_demean)
-            ret_cp = t_pc_mean - np.dot(ret_R, y_ocp_mean)
-
-            diff = t_pc_demean - np.dot(ret_R, y_ocp_demean)
-            diff_var = np.sqrt(np.var(diff))
-            diff_mask = (np.linalg.norm(diff, axis=0) < diff_var * 1.96).astype(np.bool)
-
-    return ret_cp.ravel(), ret_R
-
-
-def ransac_estimation(y_ocp_nonzero, t_pc_nonzero, n_ransac=50):
-    # thre = np.sqrt(np.sum(np.var(y_cp_nonzero - y_cp_mean[:, np.newaxis], axis=1)))
-    max_cnt = -1
-    max_inlier_mask = np.empty(t_pc_nonzero.shape[1])
-    rand_sample = np.array(
-        np.random.randint(0, len(max_inlier_mask), (n_ransac, 3)))
-
-    _R = np.empty((3,3))
-    _t = np.empty(3)
-
-    random_ocp = y_ocp_nonzero[:,rand_sample]
-    random_ocp_mean = np.mean(random_ocp, axis=2)
-
-    random_pc = t_pc_nonzero[:, rand_sample]
-    random_pc_mean = np.mean(random_pc, axis=2)
-
-    for i_ransac in six.moves.range(n_ransac):
-        random_ocp_demean = random_ocp[:, i_ransac] - random_ocp_mean[:, i_ransac]
-        random_pc_demean = random_pc[:, i_ransac] - random_pc_mean[:, i_ransac]
-
-        _R = calc_rot_by_svd(random_pc_demean, random_ocp_demean)
-        _t = random_pc_mean[:, i_ransac] - np.dot(_R, random_ocp_mean[:, i_ransac])
-        ## count inliers
-        # thre = np.std(
-        #     t_pc_demean - np.dot(_R, y_ocp_demean), axis=1)[:, np.newaxis]
-        thre = 0.025
-        inlier_mask3d = (
-            np.abs(t_pc_nonzero - np.dot(_R, y_ocp_nonzero) \
-                   - _t[:, np.newaxis]) < thre * 2)
-        inlier_mask = np.sum(inlier_mask3d, axis=0)
-        inlier_mask = (inlier_mask == 3)
-        cnt = np.sum(inlier_mask)
-        if cnt > max_cnt:
-            max_cnt = cnt
-            max_inlier_mask = inlier_mask
-
-    y_ocp_mean = np.mean(y_ocp_nonzero[:, max_inlier_mask], axis=1)
-    t_pc_mean = np.mean(t_pc_nonzero[:, max_inlier_mask], axis=1)
-    ret_R = calc_rot_by_svd(t_pc_nonzero[:, max_inlier_mask] - t_pc_mean[:, np.newaxis],
-                            y_ocp_nonzero[:, max_inlier_mask] - y_ocp_mean[:, np.newaxis])
-    ret_cp = t_pc_mean - np.dot(ret_R, y_ocp_mean)
-    return ret_cp, ret_R
 
 
 def calc_accuracy_impl(estimated_cp, estimated_ocp, estimated_R,
@@ -114,12 +49,14 @@ def calc_accuracy_impl(estimated_cp, estimated_ocp, estimated_R,
         rot_acc = 180
     return cp_acc, ocp_acc, rot_acc
 
+
 class ModelBaseConsensusAccuracy(function.Function):
 
     ## Support multi-class, one instance per one class
 
     def __init__(self, pc_path, eps=0.2, cp_eps=0.7,
                  distance_sanity=0.1, min_distance=0.015,
+                 base_path = '$HOME/workspace/cp-net/train_data/OcclusionChallengeICCV2015/models_pcd',
                  objs =['Ape', 'Can', 'Cat', 'Driller', 'Duck', 'Eggbox', 'Glue', 'Holepuncher'],
                  method="SVD", ver2=False):
         self.eps = eps
@@ -128,12 +65,19 @@ class ModelBaseConsensusAccuracy(function.Function):
         self.min_distance = min_distance
         self.method = method
         self.ver2 = ver2
+
+        ## for flann
+        self.models_flann_idx = []
         self.models_pc = []
         self.objs = objs
         for obj_name in obj_names:
-            pc = pypcd.PointCloud.from_path(os.path.join(base_name,
-                                                         'cloud_' + obs_name + '.pcd'))
-            model_pc.append(pc.pc_data)
+            pc = pypcd.PointCloud.from_path(
+                os.path.join(base_name, obs_name + '.pcd'))
+            search_idx = pyflann.FLANN()
+            search_idx.bulid_idx(pc.pc_data, algorithm='kmeans',
+                                 centers_init='kmeanspp', random_seed=1234)
+            self.flann_search_idx.append(search_idx)
+            self.models_pc.append(pc.pc_data)
 
     def forward(self, inputs):
         y_cls, y_cp, y_ocp, t_cp, t_rot, t_pc = inputs
@@ -205,44 +149,77 @@ class ModelBaseConsensusAccuracy(function.Function):
         y_cp_reshape = (y_cp + t_pc).reshape(batch_size, 3, -1)
         t_pc_reshape = t_pc.reshape(batch_size, 3, -1)
         y_ocp_reshape = y_ocp.reshape(batch_size, 3, -1)
-        # print np.sum(pred_mask.reshape(batch_size, n_class -1, -1), axis=2)
+
         for i_b in six.moves.range(batch_size):
             for i_c in six.moves.range(n_class - 1):
-                if np.sum(pred_mask[i_b, i_c]) < 50:
-                    estimated_cp[i_b, i_c] = penalty
-                    estimated_ocp[i_b, i_c] = penalty
-                    estimated_R[i_b, i_c] = np.diag((1,1,1))
-                else:
-                    pmask = pred_mask[i_b, i_c].ravel().astype(np.bool)
-                    y_cp_nonzero = (y_cp[i_b] + t_pc[i_b]).reshape(3, -1)[:, pmask]
-                    y_cp_mean = np.mean(y_cp_nonzero, axis=1, keepdims=True)
+                if np.sum(pred_mask[i_b, i_c]) < 10:
+                    continue
+                pmask = pred_mask[i_b, i_c].ravel().astype(np.bool)
+                y_cp_nonzero = y_cp_reshape[:, pmask]
+                y_cp_mean = np.mean(y_cp_nonzero, axis=1, keepdims=True)
 
-                    ## Remove outlier direct Center point
-                    cp_demean = y_cp_nonzero - y_cp_mean
-                    cp_mask = (np.linalg.norm(cp_demean, axis=0) < np.std(cp_demean) * 3)
+                ## Remove outlier direct Center point
+                cp_demean = y_cp_nonzero - y_cp_mean
+                cp_mask = (np.linalg.norm(cp_demean, axis=0) < np.std(cp_demean) * 3)
+                if np.sum(cp_mask) < 10:
+                    continue
 
-                    t_pc_nonzero = t_pc_reshape[i_b][:, pmask][:, cp_mask]
-                    y_ocp_nonzero = y_ocp_reshape[i_b][:, pmask][:, cp_mask]
+                t_pc_nonzero = t_pc_reshape[i_b][:, pmask][:, cp_mask]
+                y_ocp_nonzero = y_ocp_reshape[i_b][:, pmask][:, cp_mask]
+                y_cp_nonzero = y_cp_reshape[i_b][:, pmask][:, cp_mask]
+                y_cp_nonzero2 = (y_cp_reshape - t_pc_reshape)[i_b][:, pmask][:, cp_mask]
+                t_pc_nonzero =  t_pc_reshape[i_b][:, pmask][:, cp_mask]
 
-                    if self.method == 'SVD':
-                        estimated_ocp[i_b, i_c], estimated_R[i_b, i_c] = svd_estimation(y_ocp_nonzero, t_pc_nonzero)
+                ## flann refinement
+                num_nn = 3
+                flann_ret, flann_dist = self.flann_search_idx[i_c].nn_index(y_ocp_nonzero, num_nn)
+                flann_mask = (flann_dist < 0.03) # threshold is tmp
+                flann_ocp = self.model_pc[i_c][:, flann_ret]
 
-                    elif self.method == "RANSAC":
-                        estimated_ocp[i_b, i_c], estimated_R[i_b, i_c] = ransac_estimation(y_ocp_nonzero,
-                                                                                           t_pc_nonzero, n_ransac=50)
-                    elif self.method == "DUAL":
-                        y_cp_nonzero = y_cp_reshape[i_b][:, pmask][:, cp_mask]
-                        y_cp_nonzero2 = (y_cp_reshape - t_pc_reshape)[i_b][:, pmask][:, cp_mask]
-                        t_pc_nonzero =  t_pc_reshape[i_b][:, pmask][:, cp_mask]
+                ## ransac base estimation of rotation
+                max_cnt = -1
+                ransac_thre = 0.025
+                max_inlier_mask = np.empty(t_pc_nonzero.shape[1])
+                rand_sample = np.array(
+                    np.random.randint(0, len(max_inlier_mask), (n_ransac, 3)))
 
-                        # diff_norm = np.linalg.norm(y_cp_nonzero2, axis=0) - np.linalg.norm(y_ocp_nonzero, axis=0)
-                        # weight = np.exp(- diff_norm) / np.sum(np.exp(- diff_norm))
+                _R = np.empty((3,3))
+                _t = np.empty(3)
 
-                        estimated_R[i_b, i_c] = calc_rot_by_svd(- y_cp_nonzero2, y_ocp_nonzero)
-                        cp_concat = np.hstack([y_cp_nonzero, t_pc_nonzero + np.dot(estimated_R[i_b, i_c], - y_ocp_nonzero)])
-                        estimated_ocp[i_b, i_c] = np.mean(cp_concat, axis=1)
+                random_ocp = y_ocp_nonzero[:,rand_sample]
+                random_ocp_mean = np.mean(random_ocp, axis=2)
 
-        ret_cp, ret_ocp, ret_rot = calc_accuracy_impl(estimated_cp, estimated_ocp, estimated_R, t_cp, t_rot, batch_size, n_class)
+                random_pc = t_pc_nonzero[:, rand_sample]
+                random_pc_mean = np.mean(random_pc, axis=2)
+
+                for i_ransac in six.moves.range(n_ransac):
+                    random_ocp_demean = random_ocp[:, i_ransac] - random_ocp_mean[:, i_ransac]
+                    random_pc_demean = random_pc[:, i_ransac] - random_pc_mean[:, i_ransac]
+
+                    _R = calc_rot_by_svd(random_pc_demean, random_ocp_demean)
+                    _t = random_pc_mean[:, i_ransac] - np.dot(_R, random_ocp_mean[:, i_ransac])
+                    ## count inliers
+                    # thre = np.std(
+                    #     t_pc_demean - np.dot(_R, y_ocp_demean), axis=1)[:, np.newaxis]
+                    inlier_mask3d = (
+                        np.abs(t_pc_nonzero - np.dot(_R, y_ocp_nonzero) \
+                               - _t[:, np.newaxis]) < ransac_thre)
+                    inlier_mask = np.sum(inlier_mask3d, axis=0)
+                    inlier_mask = (inlier_mask == 3)
+                    cnt = np.sum(inlier_mask)
+                    if cnt > max_cnt:
+                        max_cnt = cnt
+                        max_inlier_mask = inlier_mask
+
+                y_ocp_mean = np.mean(y_ocp_nonzero[:, max_inlier_mask], axis=1)
+                t_pc_mean = np.mean(t_pc_nonzero[:, max_inlier_mask], axis=1)
+                ret_R = calc_rot_by_svd(t_pc_nonzero[:, max_inlier_mask] - t_pc_mean[:, np.newaxis],
+                                        y_ocp_nonzero[:, max_inlier_mask] - y_ocp_mean[:, np.newaxis])
+                ret_cp = t_pc_mean - np.dot(ret_R, y_ocp_mean)
+
+
+        ret_cp, ret_ocp, ret_rot = calc_accuracy_impl(estimated_cp, estimated_ocp,
+                                                      estimated_R, t_cp, t_rot, batch_size, n_class)
 
         return np.asarray(ret_cp, dtype=y_cp.dtype), np.asarray(ret_ocp, dtype=y_ocp.dtype), np.asarray(ret_rot, dtype=y_ocp.dtype)
 
