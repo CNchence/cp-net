9e4cbf87ec1ceeb9e928c0ccb2781701c22b02f0
oshiro <la.fete.du.travail@gmail.com>
2017-11-03 17:23:04 +0900

    refactoring generate willow dataset

--- scripts/generate_train_data_from_willow_dataset.py
+++ scripts/generate_train_data_from_willow_dataset.py
@@ -1,89 +1,73 @@
 #!/usr/bin/env python
 
 import warnings
-import pypcd
 import os
-
+import glob
+import argparse
 import cv2
 import numpy as np
-
-num_class = 35
-num_view = 37
 
 img_width = 640
 img_height = 480
 
-
-orig_data_path = os.path.join(os.getcwd(), '../../download_data/willow_models')
-dataset_path = os.path.join(os.getcwd(), '../../train_data/willow_models')
-
-# 1 . generate RGB image
-# 2 . generate depth image
-# 3 . generate mask image
-# 4 . generate cp-dist-map
-
 def main():
-    if not os.path.exists(orig_data_path):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('original_data_path')
+    parser.add_argument('generated_data_path')
+    args = parser.parse_args()
+    orig_datapath = args.original_data_path
+    gen_datapath = args.generated_data_path
+
+    if not os.path.exists(orig_datapath):
         warnings.warn('Please install willow dataset')
         return False
-    if not os.path.exists(dataset_path):
-        os.makedirs(dataset_path, mode=0755)
+    if not os.path.exists(gen_datapath):
+        os.makedirs(gen_datapath, mode=0755)
 
-    for i in range(num_class):
-        c_idx =  '{0:02d}'.format(i+1)
-        c_name = 'object_' + c_idx
-        orig_c_path = os.path.join(orig_data_path, c_name, 'views')
-        c_path = os.path.join(dataset_path, c_name)
-        if not os.path.exists(c_path):
-            os.makedirs(c_path, mode=0755)
+    cls_view_path_mask = os.path.join(orig_datapath, 'object_{0:02d}', 'views')
+    cls_out_mask = os.path.join(gen_datapath, 'object_{0:02d}')
+    cloud_name = 'cloud_{0:0>8}.pcd'
+    indices_name = 'object_indices_{0:0>8}.txt'
+    rgb_name = 'rgb_{0:0>8}.png'
+    depth_name = 'depth_{0:0>8}.png'
+    mask_name = 'mask_{0:0>8}.png'
+    pos_name = 'pos_{0:0>8}.npy'
+    rot_name = 'rot_{0:0>8}.npy'
+
+    n_class = len(glob.glob(os.path.join(orig_datapath, 'object_*')))
+    for i in range(n_class):
+        in_fpath = cls_view_path_mask.format(i + 1)
+        out_fpath = cls_out_mask.format(i + 1)
+        if not os.path.exists(out_fpath):
+            os.makedirs(out_fpath, mode=0755)
+        num_view = len(glob.glob(os.path.join(in_fpath, "cloud_*.pcd")))
         for j in range(num_view):
-            v_idx = '{0:08d}'.format(j)
-            # # 1. generate RGB image
-            # cmd1 ='pcl_pcd2png ' + os.path.join(orig_c_path, 'cloud_' + v_idx + '.pcd') + ' ' + os.path.join(c_path, 'rgb_' + v_idx + '.png')
-            # os.system(cmd1)
-            # # 2. generate RGB image
-            # cmd2 ='pcl_pcd2png --field z --scale no ' + os.path.join(orig_c_path, 'cloud_' + v_idx + '.pcd') + ' ' + os.path.join(c_path, 'depth_' + v_idx + '.png')
-            # # cmd2 ='pcl_pcd2png --field z ' + os.path.join(orig_c_path, 'cloud_' + v_idx + '.pcd') + ' ' + os.path.join(c_path, 'depth_' + v_idx + '_viz.png')
-            # os.system(cmd2)
-            # # 3 . generate mask image
-            # f = open(os.path.join(orig_c_path, 'object_indices_' + v_idx + '.txt'), 'r')
-            # mask_arr = np.zeros(img_width * img_height)
-            # for line in f:
-            #     mask_arr[int(line)] = 255
-            # mask = mask_arr.reshape(img_height, img_width)
-            # cv2.imwrite(os.path.join(c_path, 'mask_' + v_idx + '.png'), mask)
-
-            # generate center point (cp) distance map image
-            # load point cloud
-            pc = pypcd.PointCloud.from_path(os.path.join(orig_c_path,
-                                                         'cloud_' + v_idx + '.pcd'))
-            # load pose text
-            f = open(os.path.join(orig_c_path, 'pose_' + v_idx + '.txt'), 'r')
+            print "genrating data class:{} view:{}".format(i + 1, j)
+            # 1. generate RGB image
+            cmd1 ='pcl_pcd2png {} {}'.format(os.path.join(in_fpath, cloud_name.format(j)),
+                                             os.path.join(out_fpath, rgb_name.format(j)))
+            os.system(cmd1)
+            # 2. generate depth image
+            cmd2 ='pcl_pcd2png --field z --scale no {} {}'.format(os.path.join(in_fpath, cloud_name.format(j)),
+                                                                  os.path.join(out_fpath, depth_name.format(j)))
+            os.system(cmd2)
+            # 3 . generate mask image
+            f = open(os.path.join(in_fpath, indices_name.format(j)), 'r')
+            mask_arr = np.zeros(img_width * img_height)
+            for line in f:
+                mask_arr[int(line)] = 255
+            mask = mask_arr.reshape(img_height, img_width)
+            cv2.imwrite(os.path.join(out_fpath, mask_name.format(j)), mask)
+            # 4. save pos and rot
+            f = open(os.path.join(in_fpath, 'pose_' + v_idx + '.txt'), 'r')
             pose_arr = []
             for line in f:
                 pose_arr = list(map(float,line.split(" ")))
-            pose_mat = np.array(pose_arr).reshape(4,4)
+            pose_mat = np.asarray(pose_arr).reshape(4,4)
             pos = np.array([pose_mat[0][3], pose_mat[1][3], pose_mat[2][3]])
             rot = pose_mat[0:3,0:3]
-            organized_pc = np.zeros(3 * img_height * img_width).reshape(img_height, img_width, 3)
-            dist_map = np.zeros(3 * img_height * img_width).reshape(img_height, img_width, 3)
-            for i in range(img_height):
-                for j in range(img_width):
-                    for k in range(3):
-                        organized_pc[i][j][k] = pc.pc_data[i * img_width + j][k]
-                        dist_map[i][j][k] = pos[k] - pc.pc_data[i * img_width + j][k]
-            # # save dist map
-            # np.save(os.path.join(c_path, 'dist_' + v_idx + '.npy'), dist_map)
-            # # save center point position
-            # np.save(os.path.join(c_path, 'pos_' + v_idx + '.npy'), pos)
-            # # save rotation_matrix numpy
-            # np.save(os.path.join(c_path, 'rot_' + v_idx + '.npy'), rot)
-            # save organized point cloud as numpy array
-            np.save(os.path.join(c_path, 'pc_' + v_idx + '.npy'), organized_pc)
-            print 'saving : ' + os.path.join(c_path, 'pc_' + v_idx + '.npy')
-            # print pose_mat
-            # print pos
-            # print rot
-            
+            np.save(os.path.join(out_fpath, pos_name.format(j)), pos)
+            np.save(os.path.join(out_fpath, rot_name.format(j)), rot)
+
 if __name__ == '__main__':
     main()
